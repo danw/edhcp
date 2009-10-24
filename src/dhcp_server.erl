7df952549d400243ade17ce36da71821a6d3e80b
Dan Willemsen <dan@danw.org>
2009-10-24 01:15:44 -0700

    Add file headers

--- src/dhcp_server.erl
+++ src/dhcp_server.erl
@@ -1,3 +1,27 @@
+%%%-------------------------------------------------------------------
+%%% File    : dhcp_server.erl
+%%% Author  : Dan Willemsen <dan@csh.rit.edu>
+%%% Purpose : 
+%%%
+%%% edhcp, Copyright (C) 2009 Dan Willemsen
+%%%
+%%% This program is free software; you can redistribute it and/or
+%%% modify it under the terms of the GNU General Public License as
+%%% published by the Free Software Foundation; either version 2 of the
+%%% License, or (at your option) any later version.
+%%%
+%%% This program is distributed in the hope that it will be useful,
+%%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+%%% General Public License for more details.
+%%%                         
+%%% You should have received a copy of the GNU General Public License
+%%% along with this program; if not, write to the Free Software
+%%% Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+%%% 02111-1307 USA
+%%%
+%%%-------------------------------------------------------------------
+
 -module (dhcp_server).
 -behaviour (gen_server).
 
